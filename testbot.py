192cb322b16b36c35f0c938396c8c5a43cc52538
theqa-ninja <qaninja@puzzlebang.com>
2020-12-30 18:22:39 -0600

    for heroku

--- testbot.py
+++ testbot.py
@@ -1,11 +1,15 @@
 import discord
 import json
 import pdb
+import os
 
-with open('config.json', 'r') as f:
-    config = json.load(f)
+try:
+    with open('config.json', 'r') as f:
+        config = json.load(f)
 
-token = config['bottoken']
+    token = config['bottoken']
+except FileNotFoundError:
+    token = os.environ['bottoken']
 
 
 class MyClient(discord.Client):
@@ -25,22 +29,21 @@ class MyClient(discord.Client):
         if message.content.startswith('!hello'):
             await message.channel.send('Hello!')
 
-        if message.content.startswith('!channel'):
-            found = True
-            await message.channel.send(f'hey {message.channel.mention}! your channel info is {message.channel}')
-
-        if message.content.startswith('!author'):
-            found = True
-            await message.channel.send(f'hey {message.author.mention}! Your discord id is {message.author}')
-
-        # if message.content.contains('rickroll'):
-        #     found = True
-        #     await message.channel.send(f'Here ya go! https://www.youtube.com/watch?v=xn38dg0YrzY')
-
-        if message.content.startswith('!tdb'):
+        elif message.content.startswith('!tdb'):
             pdb.set_trace()
             await message.channel.send(f'Hello {message.author}!')
             await message.channel.send('testing stuff')
+        elif message.content.startswith('!channel'):
+            found = True
+            await message.channel.send(f'hey {message.channel.mention}! your channel info is {message.channel}')
+
+        elif message.content.startswith('!author'):
+            found = True
+            await message.channel.send(f'hey {message.author.mention}! Your discord id is {message.author}')
+
+        elif message.content.contains('rickroll'):
+            found = True
+            await message.channel.send(f'Here ya go! https://www.youtube.com/watch?v=xn38dg0YrzY')
 
         if found:
             await message.delete
